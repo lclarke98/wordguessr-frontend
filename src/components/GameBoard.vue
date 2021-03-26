07b49d91467fd30a066dc75622f0436aec8d7bad
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-03-26 14:25:33 +0000

    update: minor changes

--- src/components/GameBoard.vue
+++ src/components/GameBoard.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
-    <h1>Hi {{userInfo.username}}, guess the word...</h1>
-    <h2>Number of guesses remaining: {{guessCount}}</h2>
+    <h1>Hi {{ userInfo.username }}, guess the word...</h1>
+    <h2>Number of guesses remaining: {{ guessCount }}</h2>
     <form @submit.prevent="makeGuess">
       <label>
-        <input type="text" placeholder="Make guess" v-model="guess" pattern="[a-z]{1}" >
+        <input
+          type="text"
+          placeholder="Make guess"
+          v-model="guess"
+          pattern="[a-z]{1}"
+        />
       </label>
       <button>Make Guess</button>
     </form>
@@ -13,48 +18,65 @@
       <div id="correct-container">
         <h1>The Word</h1>
         <div v-if="list.length === 0">
-            <ul>
-              <li v-for="char in word" style="padding: 10px; display: inline">_</li>
-            </ul>
-        </div>
-        <div v-else>
-              <ul>
-                <li v-for="char in word" v-if="char.display === true" style="padding: 10px; display: inline"> {{char.letter}} </li>
-                <li v-else-if="char.display === false" style="padding: 10px; display: inline"> _ </li>
-              </ul>
-            </div>
-        </div>
-      </div>
-
-      <ul>
-      </ul>
-
-      <div id="incorrect-container">
-        <h1>Incorrect guesses</h1>
-        <div v-if="list.length === 0">
-          No guesses yet
+          <ul>
+            <li v-for="char in word" style="padding: 10px; display: inline">
+              _
+            </li>
+          </ul>
         </div>
         <div v-else>
           <ul>
-            <li v-for="letter in list" :key="letter.guess" v-if="letter.correct === false" style="padding: 10px; display: inline">{{letter.guess}}</li>
+            <li
+              v-for="char in word"
+              v-if="char.display === true"
+              style="padding: 10px; display: inline"
+            >
+              {{ char.letter }}
+            </li>
+            <li
+              v-else-if="char.display === false"
+              style="padding: 10px; display: inline"
+            >
+              _
+            </li>
           </ul>
         </div>
       </div>
-  </div>
+    </div>
 
+    <ul></ul>
+
+    <div id="incorrect-container">
+      <h1>Incorrect guesses</h1>
+      <div v-if="list.length === 0">
+        No guesses yet
+      </div>
+      <div v-else>
+        <ul>
+          <li
+            v-for="letter in list"
+            :key="letter.guess"
+            v-if="letter.correct === false"
+            style="padding: 10px; display: inline"
+          >
+            {{ letter.guess }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
-import UserInfoStore from "@/app/user-info-store";
-import gc from "@/app/game-controller";
-import axios from 'axios'
+import UserInfoStore from '@/app/user-info-store';
+import gc from '@/app/game-controller';
+import axios from 'axios';
 const API_BASE_URL = process.env.VUE_APP_API_BASE_URL;
 
 export default {
-name: "GameBoard.vue",
+  name: 'GameBoard.vue',
   data: function() {
-    return{
+    return {
       userPoolId: process.env.VUE_APP_COGNITO_USERPOOL_ID,
       userInfo: UserInfoStore.state.cognitoInfo,
       isLoading: false,
@@ -62,40 +84,43 @@ name: "GameBoard.vue",
       list: '',
       word: '',
       fullWord: '',
-      guessCount: 0,
-    }
+      guessCount: 0
+    };
   },
   methods: {
-    async getGame(){
-      axios({
-        method:'get',
-        url: API_BASE_URL + '/game/game',
-        params: {
-          gameID: new URL(location.href).searchParams.get('gameID'),
-        },
-        headers: {
-          'Content-Type': 'application/json'
-        },
-      })
-          .then(response => {
-            this.isLoading = false
-            //this.$emit('completed', response.data.data)
-            this.guessCount = response.data[0].guess_count
-            this.fullWord = response.data[0].word
-            this.list = response.data[1]
-            this.word = gc.splitWord(response.data[0].word, this.list)
-          })
-          .catch(error => {
-            this.errors = error.response.data.errors
-            this.isLoading = false
-          })
+    async getGame() {
+      // axios({
+      //   method:'get',
+      //   url: API_BASE_URL + '/game/game',
+      //   params: {
+      //     gameID: new URL(location.href).searchParams.get('gameID'),
+      //   },
+      //   headers: {
+      //     'Content-Type': 'application/json'
+      //   },
+      // })
+      //     .then(response => {
+      //       this.isLoading = false
+      //       //this.$emit('completed', response.data.data)
+      //       this.guessCount = response.data[0].guess_count
+      //       this.fullWord = response.data[0].word
+      //       this.list = response.data[1]
+      //       this.word = gc.splitWord(response.data[0].word, this.list)
+      //     })
+      //     .catch(error => {
+      //       this.errors = error.response.data.errors
+      //       this.isLoading = false
+      //     })
     },
 
-    async makeGuess(){
-      if (! this.fullWord.includes(this.guess)){
-        this.guessCount -=1
+    async makeGuess() {
+      if (!this.fullWord.includes(this.guess)) {
+        this.guessCount -= 1;
       }
-      if (await gc.validateGuess(this.guess,this.list) === true && this.guess !== ""){
+      if (
+        (await gc.validateGuess(this.guess, this.list)) === true &&
+        this.guess !== ''
+      ) {
         axios({
           method: 'post',
           url: API_BASE_URL + '/game/guessLetter',
@@ -106,37 +131,35 @@ name: "GameBoard.vue",
           },
           headers: {
             'Content-Type': 'application/json'
-          },
+          }
         })
-            .then(response => {
-              this.guess = ''
-              this.isLoading = false
-              this.list = response.data[1]
-              this.word = gc.splitWord(response.data[0].word, this.list)
-              //this.$emit('completed', response.data.data)
-              if (response.data[2].complete === true){
-                alert("Game Over you won")
-              }else if (this.guessCount === 0){
-                alert("game over you lost")
-              }
-            })
-            .catch(error => {
-              // handle authentication and validation errors here
-              this.errors = error.response.data.errors
-              this.isLoading = false
-            })
-      }else {
-        alert("Guess already made")
-        this.guess = ""
+          .then(response => {
+            this.guess = '';
+            this.isLoading = false;
+            this.list = response.data[1];
+            this.word = gc.splitWord(response.data[0].word, this.list);
+            //this.$emit('completed', response.data.data)
+            if (response.data[2].complete === true) {
+              alert('Game Over you won');
+            } else if (this.guessCount === 0) {
+              alert('game over you lost');
+            }
+          })
+          .catch(error => {
+            // handle authentication and validation errors here
+            this.errors = error.response.data.errors;
+            this.isLoading = false;
+          });
+      } else {
+        alert('Guess already made');
+        this.guess = '';
       }
-    },
+    }
   },
-  beforeMount(){
-    this.getGame()
-  },
-}
+  beforeMount() {
+    this.getGame();
+  }
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/components/GameController.vue
+++ src/components/GameController.vue
@@ -2,50 +2,47 @@
   <div>
     <button v-on:click="createGame('normal')">Normal</button>
   </div>
-
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios';
 import UserInfoStore from '../app/user-info-store';
 const API_BASE_URL = process.env.VUE_APP_API_BASE_URL;
 export default {
-  name: "GameController",
+  name: 'GameController',
 
   data: function() {
-    return{
+    return {
       userPoolId: process.env.VUE_APP_COGNITO_USERPOOL_ID,
       userInfo: UserInfoStore.state.cognitoInfo
-    }
+    };
   },
 
   methods: {
-  async createGame(mode) {
-
-    axios({
-      method: 'post',
-      url: API_BASE_URL + '/game/createGame',
-      data: {
-        sub: this.userInfo.sub,
-        mode: mode,
-      },
-      headers: {
-        'Content-Type': 'application/json'
-      },
-    })
-        .then(response => {
-          window.location.href = 'StartGame?gameID=' + response.data.gameID;
-        })
-        .catch(error => {
-          // handle authentication and validation errors here
-          this.errors = error.response.data.errors
-          this.isLoading = false
-        })
+    async createGame() {
+      window.location.href = 'StartGame?gameID=52';
+      // axios({
+      //   method: 'post',
+      //   url: API_BASE_URL + '/game/createGame',
+      //   data: {
+      //     sub: this.userInfo.sub,
+      //     mode: mode,
+      //   },
+      //   headers: {
+      //     'Content-Type': 'application/json'
+      //   },
+      // })
+      //     .then(response => {
+      //       window.location.href = 'StartGame?gameID=' + response.data.gameID;
+      //     })
+      //     .catch(error => {
+      //       // handle authentication and validation errors here
+      //       this.errors = error.response.data.errors
+      //       this.isLoading = false
+      //     })
+    }
   }
-}
-}
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
