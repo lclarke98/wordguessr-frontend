5b771d6f55c73f7e9e2788d270b7c8bf3b583aca
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-03-26 14:25:58 +0000

    updated Home.vue and created Home.scss

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,63 +1,63 @@
 <template>
   <div class="row">
     <div class="col">
-      <h3>Welcome, </h3>
+      <h1>WordGuessr</h1>
       <div class="alert alert-info">
-        {{userInfo}}
+        {{ userInfo }}
       </div>
 
-      <ul>
-        <li><router-link to="/game">New Game</router-link></li>
-        <li><router-link to="/logout">Logout</router-link></li>
-      </ul>
+      <div class="btn__container">
+        <router-link class="btn" to="/game">New Game</router-link>
+        <router-link class="btn" to="/logout">Logout</router-link>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import './Home.scss';
+import '../styles/buttons/buttons.scss';
 const API_BASE_URL = process.env.VUE_APP_API_BASE_URL;
 import UserInfoStore from '../app/user-info-store';
-import axios from "axios";
+import axios from 'axios';
 export default {
-
   name: 'Home',
   data: function() {
-    return{
+    return {
       userPoolId: process.env.VUE_APP_COGNITO_USERPOOL_ID,
       userInfo: UserInfoStore.state.cognitoInfo
-    }
+    };
   },
 
-  methods:{
-    checkUser(){
+  methods: {
+    checkUser() {
       axios({
         method: 'post',
         url: API_BASE_URL + '/user/user',
         data: {
-          sub: this.userInfo.sub,
+          sub: this.userInfo.sub
         },
         headers: {
           'Content-Type': 'application/json'
-        },
+        }
       })
-          .then(response => {
-            this.guess = ''
-            this.isLoading = false
-            this.$emit('completed', response.data.data)
-          })
-          .catch(error => {
-            // handle authentication and validation errors here
-            this.errors = error.response.data.errors
-            this.isLoading = false
-          })
+        .then(response => {
+          this.guess = '';
+          this.isLoading = false;
+          this.$emit('completed', response.data.data);
+        })
+        .catch(error => {
+          // handle authentication and validation errors here
+          this.errors = error.response.data.errors;
+          this.isLoading = false;
+        });
     }
   },
 
-  beforeMount(){
-    this.checkUser()
-  },
-}
+  beforeMount() {
+    this.checkUser();
+  }
+};
 </script>
 
-<style>
-</style>
+<style></style>
