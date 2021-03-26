e47a397ef19cdf729cf25589f886b1fe5bd09967
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-03-26 14:25:46 +0000

    created Header.vue and Header.scss

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <div class="header__content">
+      <div class="header__logo"><h2>wordguessr</h2></div>
+      <div class="header__profile">
+        <font-awesome-icon icon="user-circle" />
+      </div>
+      <div class="btn__container">
+        <router-link class="header__logout-btn btn" to="/logout"
+          >Logout</router-link
+        >
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import './Header.scss';
+
+export default {
+  name: 'Header'
+};
+</script>
+
+<style></style>
