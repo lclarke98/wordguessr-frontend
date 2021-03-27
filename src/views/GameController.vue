e429481d5b0a608a5087e9e63ee86125fab1eb16
Sam-RP1 <44971801+Sam-RP1@users.noreply.github.com>
2021-03-27 17:38:22 +0000

    update: minor changes

--- src/views/GameController.vue
+++ src/views/GameController.vue
@@ -1,19 +1,19 @@
 <template>
-
-  <GameController>
-    <h1>Start Game</h1>
-  </GameController>
-
+  <div class="container">
+    <GameController>
+      <h1>Start Game</h1>
+    </GameController>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import GameController from '@/components/GameController.vue'
+import GameController from '@/components/GameController.vue';
 
 export default {
   name: 'Game',
   components: {
     GameController
   }
-}
+};
 </script>
